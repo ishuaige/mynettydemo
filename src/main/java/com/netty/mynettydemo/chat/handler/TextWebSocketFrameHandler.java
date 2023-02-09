24d24c44d2b2bcfbfccd5990416c5289fa994b17
ishuaige <1394293788@qq.com>
2023-02-10 00:29:51 +0800

    add:优化聊天室，修改聊天室bug

--- src/main/java/com/netty/mynettydemo/chat/handler/TextWebSocketFrameHandler.java
+++ src/main/java/com/netty/mynettydemo/chat/handler/TextWebSocketFrameHandler.java
@@ -1,21 +1,38 @@
 package com.netty.mynettydemo.chat.handler;
 
+import com.netty.mynettydemo.chat.config.NettyConfig;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 用来解析websocket连接时的url参数
  * 参考：https://blog.csdn.net/mfkarj/article/details/103279559
  * @author niumazlb
  */
+@Slf4j
+@ChannelHandler.Sharable
 public class TextWebSocketFrameHandler extends
 		SimpleChannelInboundHandler<TextWebSocketFrame> {
 
 	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		//有新的连接加入，就加到我们的连接组里
+		Channel channel = ctx.channel();
+		NettyConfig.group.add(channel);
+	}
+
+	/**
+	 * 解析数据。第一次url参数也是走这个方法
+	 * 注意：一定要回调父类该方法。不然内存会泄露。不要以为重写该方法后，在最后面添加清除ByteBuf告诉你这是无用的.
+	 */
+	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		if (msg instanceof FullHttpRequest) {
+		if (null != msg && msg instanceof FullHttpRequest) {
 			FullHttpRequest request = (FullHttpRequest) msg;
 			String uri = request.uri();
 			String origin = request.headers().get("Origin");
@@ -38,9 +55,27 @@ public class TextWebSocketFrameHandler extends
 		super.channelRead(ctx, msg);
 	}
 
+	/**
+	 * 处理每次发送过来的数据。
+	 * @param channelHandlerContext
+	 * @param msg
+	 * @throws Exception
+	 */
 	@Override
-	protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
-
+	protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame msg) throws Exception {
+		// 获取接收到的消息
+		String content = msg.text();
+		log.info("接收到的消息：{}" , content);
+		for (Channel userChannel : NettyConfig.group) {
+			//将消息转换成 TextWebSocketFrame 才能被web接收到
+			TextWebSocketFrame webSocketFrame = new TextWebSocketFrame(content);
+			userChannel.writeAndFlush(webSocketFrame);
+		}
 	}
 
+	@Override
+	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+		Channel channel = ctx.channel();
+		NettyConfig.group.remove(channel);
+	}
 }
--- src/main/java/com/netty/mynettydemo/chat/handler/WebSocketServerInitializer.java
+++ src/main/java/com/netty/mynettydemo/chat/handler/WebSocketServerInitializer.java
@@ -32,11 +32,9 @@ public class WebSocketServerInitializer extends ChannelInitializer<SocketChannel
                 .addLast("aggregator", new HttpObjectAggregator(65536))
                 // 对写大数据流的支持
                 .addLast("http-chunked", new ChunkedWriteHandler())
-                //用来解析建立websocket连接时url携带的参数
+                //自己定义的消息处理器,用来解析建立websocket连接时url携带的参数,也可以用来处理收到的消息,一定要在protocolHandler之前
                 .addLast(new TextWebSocketFrameHandler())
                 // websocket服务器处理的协议  并且用于指定给客户端连接访问的路由：/ws
-                .addLast("protocolHandler",new WebSocketServerProtocolHandler("/ws"))
-                // 自己定义的消息处理器
-                .addLast("chatHandler", new ChatHandler());
+                .addLast("protocolHandler",new WebSocketServerProtocolHandler("/ws"));
     }
 }
